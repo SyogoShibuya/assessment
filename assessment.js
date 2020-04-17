cefbbc3ec073248ee6cd7526d0132f3404df54b8
cocoa <40324411+SyogoShibuya@users.noreply.github.com>
2020-04-17 22:13:45 +0900

    #1 に対する対応を行いました

--- assessment.js
+++ assessment.js
@@ -52,6 +52,7 @@ assessmentButton.onclick = () => {
   tweetDivided.appendChild(script);
 };
 
+
 const answers = [
   '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
